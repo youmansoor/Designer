f9bb982345d5f9ba442ad794e39786ffc70d63b7
youmansoor <mansooryousuf56@gmail.com>
2025-06-15 15:58:40 +0500

    Update

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    toggleButton.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
